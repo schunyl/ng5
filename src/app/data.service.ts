c4a799fd8c1dc011ecc456d0799bb4e0c7809c18
Sunil Adhikari <sunil.adhikari@itonics.de>
2018-06-08 16:35:02 +0545

    First Commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DataService {
+
+  private goals = new BehaviorSubject<any>(['My First Goal', 'My Second Goal', 'My Third Goal']);
+  goal = this.goals.asObservable();
+
+  constructor() { }
+
+  changeGoal(goal){
+    this.goals.next(goal);
+  }
+}
